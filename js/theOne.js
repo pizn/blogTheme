11ae806f98cd89295d2b8aa640c4b9ff95de8462
pizn <pizner@gmail.com>
2012-11-13 13:21:51 +0800

    fix

--- js/theOne.js
+++ js/theOne.js
@@ -10,9 +10,7 @@ var navs, url, cur, i;
 navs = $("#nav li a");
 for(i = 1 ; i < navs.length; i++ ){
     url = navs[i].href;
-    console.log(url);
     cur = window.location.href;
-    console.log(cur);
     if(cur.indexOf(url) !=-1) {
         navs[i].className = "action";
         navs[0].className = "";
